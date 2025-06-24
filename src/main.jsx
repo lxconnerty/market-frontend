3a817d6d65954ea813327af9ce4356a3048c3fd4
Vladislav <kolunov_maks_8@mail.ru>
2025-06-24 04:52:23 +0300

    fix: правки index.html, main.jsx, vite.config.js

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './market_frontend_app.js'
+import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
   </StrictMode>,
 )
+
+
