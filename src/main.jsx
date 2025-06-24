11f3a9b9bd6a47f97e3023b2729278ace40b57a8
Vladislav <kolunov_maks_8@mail.ru>
2025-06-24 04:07:29 +0300

    fix: переместил index.html в public, восстановил src

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './market_frontend_app.jsx'
+import App from './market_frontend_app.js'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
